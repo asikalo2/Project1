869385ce3d598c4f5f6fe107aa36f059309742b4
Amila Sikalo <asikalo2@etf.unsa.ba>
2018-11-03 19:34:56 +0100

    Ispravljanje izvedene klase King

--- src/ba/unsa/etf/rpr/King.java
+++ src/ba/unsa/etf/rpr/King.java
@@ -1,22 +1,31 @@
 package ba.unsa.etf.rpr;
 
 public class King extends ChessPiece {
+
+    private boolean provjeriPomak(String pozicija2){
+        if((pozicija2.charAt(1)-this.position.charAt(1)) != 2) ||
+        (pozicija2.charAt(1)-this.position.charAt(1)) != -2)) return false;
+        return true;
+    }
+
     King(String pozicija1, Color boja1) {
         super(pozicija1, boja1);
     }
 
     @Override
     public String getPosition() {
-        return null;
+        return this.position;
     }
 
     @Override
     public Color getColor() {
-        return null;
+        return this.color;
     }
 
     @Override
     public void move(String position) {
-
-    }
+        if (!ispravnostPozicije(position)) throw new IllegalArgumentException("Neispravna pozicija!");
+        if(!provjeriPomak(position, Color.WHITE)) throw new IllegalChessMoveException ("Neispravan potez!");
+            this.position=position;
+        }
 }
